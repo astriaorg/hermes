eb77ade0826abe75a3e405d764415b3ffecc1287
elizabeth <elizabethjbinks@gmail.com>
2025-03-11 12:42:21 -0400

    fmt

--- crates/relayer-cli/src/commands/listen.rs
+++ crates/relayer-cli/src/commands/listen.rs
@@ -148,7 +148,9 @@ fn subscribe(
     // Q: Should this be restricted only to backends that support it,
     // or are all backends expected to support subscriptions?
     match chain_config {
-        ChainConfig::CosmosSdk(config) | ChainConfig::Namada(config) | ChainConfig::Astria(config) => {
+        ChainConfig::CosmosSdk(config)
+        | ChainConfig::Namada(config)
+        | ChainConfig::Astria(config) => {
             let (event_source, monitor_tx) = match &config.event_source {
                 EventSourceMode::Push { url, batch_delay } => EventSource::websocket(
                     chain_config.id().clone(),
@@ -216,7 +218,9 @@ fn detect_compatibility_mode(
     rt: Arc<TokioRuntime>,
 ) -> eyre::Result<CompatMode> {
     let rpc_addr = match config {
-        ChainConfig::CosmosSdk(config) | ChainConfig::Namada(config) | ChainConfig::Astria(config) => config.rpc_addr.clone(),
+        ChainConfig::CosmosSdk(config)
+        | ChainConfig::Namada(config)
+        | ChainConfig::Astria(config) => config.rpc_addr.clone(),
         ChainConfig::Penumbra(config) => config.rpc_addr.clone(),
     };
 
@@ -225,9 +229,9 @@ fn detect_compatibility_mode(
         .build()?;
 
     let compat_mode = match config {
-        ChainConfig::CosmosSdk(config) | ChainConfig::Namada(config) | ChainConfig::Astria(config) => {
-            rt.block_on(fetch_compat_mode(&client, config))?
-        }
+        ChainConfig::CosmosSdk(config)
+        | ChainConfig::Namada(config)
+        | ChainConfig::Astria(config) => rt.block_on(fetch_compat_mode(&client, config))?,
         ChainConfig::Penumbra(config) => {
             let status = rt.block_on(client.status())?;
             penumbra::util::compat_mode_from_version(&config.compat_mode, status.node_info.version)?
